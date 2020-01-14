90a609cda9d0edf5ffa72322294ac7ed3996c578
ki99 <qldntdma87@kaist.ac.kr>
2020-01-14 20:31:51 +0900

    리셋버튼 없애고 save를 스위치로 바꿈.

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
@@ -81,12 +81,12 @@ public final class R {
         public static final int packed = 0x7f08006e;
         public static final int parent = 0x7f080070;
         public static final int percent = 0x7f080073;
-        public static final int right = 0x7f080079;
-        public static final int spread = 0x7f08009d;
-        public static final int spread_inside = 0x7f08009e;
-        public static final int start = 0x7f0800a3;
-        public static final int top = 0x7f0800ba;
-        public static final int wrap = 0x7f0800c9;
+        public static final int right = 0x7f080078;
+        public static final int spread = 0x7f08009c;
+        public static final int spread_inside = 0x7f08009d;
+        public static final int start = 0x7f0800a2;
+        public static final int top = 0x7f0800b9;
+        public static final int wrap = 0x7f0800c8;
     }
     public static final class styleable {
         private styleable() {}
